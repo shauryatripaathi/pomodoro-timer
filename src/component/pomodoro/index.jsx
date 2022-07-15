975bcb015456c28b588c65e98e214a4306a7d3be
Shaurya Tripathi <shauryatripaathi@gmail.com>
2022-07-15 12:42:21 +0530

    Update index.jsx

--- src/component/pomodoro/index.jsx
+++ src/component/pomodoro/index.jsx
@@ -4,7 +4,7 @@ import './style.css'
 
 export default function Pomodoro(){  
     const arr = ['work','short break','long break']
-    const[time,setTime] = useState(180)
+    const[time,setTime] = useState(1500)
     
     useEffect(()=>{
        if(time>0){
@@ -13,11 +13,16 @@ export default function Pomodoro(){
         },1000)
         return ()=>clearInterval(interval)
     }
+    else if(time<0){
+        setTime(180)
+        alert('times up')
+    }
     },[])
-    const getTime = (time)=>{
-        const min =  time/60;
-        const sec =  min/60  
-        return `${Math.floor(min)} : ${Math.floor(sec)}`
+    const getTime = ()=>{
+        const min =  Math.floor(time/60);
+        const sec =  time%60
+        console.log(min,sec,time,'this is time')
+        return `${min} : ${sec} `
     }
 
     return(
@@ -25,12 +30,12 @@ export default function Pomodoro(){
             <h2>Pomodoro Timer</h2>
             {arr.map((item)=>{
                 return(
-                    <button className="buttonContainer">{getTime()}</button>
+                    <button className="buttonContainer">{item}</button>
                 )
             })}
                
             <div className='timer'>
-                <div style={{fontSize:'5em'}}>{time}</div>
+                <div style={{fontSize:'4em'}}>{getTime()}</div>
                 <button>Start</button>
              </div>
         </div>
