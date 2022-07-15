d0f829a013140fc5e629f20c3542db5f2042d58f
Shaurya Tripathi <shauryatripaathi@gmail.com>
2022-07-15 23:16:57 +0530

    Update : Added reset button
    Added reset button
    changed Start/pause button style

--- src/component/pomodoro/index.jsx
+++ src/component/pomodoro/index.jsx
@@ -1,43 +1,42 @@
 import { useEffect, useState } from 'react'
-import ProgressCircle from '../ProgressCircle'
 import './style.css'
 
-export default function Pomodoro(){  
-    const arr = ['work','short break','long break']
-    const[time,setTime] = useState(1500)
-    
-    useEffect(()=>{
-       if(time>0){
-        const interval = setInterval(()=>{
-            setTime((time)=>time-1)
-        },1000)
-        return ()=>clearInterval(interval)
-    }
-    else if(time<0){
-        setTime(180)
-        alert('times up')
-    }
-    },[])
-    const getTime = ()=>{
-        const min =  Math.floor(time/60);
-        const sec =  time%60
-        console.log(min,sec,time,'this is time')
+export default function Pomodoro() {
+    const [isActive, setIsActive] = useState(false)
+    const [time, setTime] = useState(1500)
+    const [isReset,setIsReset] = useState(false)
+
+    useEffect(() => {
+        if (!isReset && isActive && time > 0) {
+            const interval = setInterval(() => {
+                setTime(time - 1)
+            }, 1000)
+            return () => clearInterval(interval)
+        }
+    }, [time, isActive,isReset])
+    const getTime = () => {
+        const min = Math.floor(time / 60);
+        const sec = time % 60
+        console.log(min, sec, time, 'this is time')
         return `${min} : ${sec} `
     }
 
-    return(
+    const toggleClock = () => {
+        setIsActive(!isActive)
+    }
+
+    const resetButton = ()=>{
+        window.location.reload()
+    }
+
+    return (
         <div>
-            <h2>Pomodoro Timer</h2>
-            {arr.map((item)=>{
-                return(
-                    <button className="buttonContainer">{item}</button>
-                )
-            })}
-               
+            <h2 style={{display:'flex',justifyContent:'center'}}>Pomodoro Timer</h2>
             <div className='timer'>
-                <div style={{fontSize:'4em'}}>{getTime()}</div>
-                <button>Start</button>
-             </div>
+                <div style={{ fontSize: '4em' }}>{getTime()}</div>
+                <button className='buttonStyle' onClick={toggleClock}>{isActive ? 'Pause' : 'Start'}</button>
+                <button onClick={resetButton}>Reset</button>
+            </div>
         </div>
     )
 }
